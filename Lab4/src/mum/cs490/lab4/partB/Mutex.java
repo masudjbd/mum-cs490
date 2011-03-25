9c619ecd09512f189e6e30fb4f22e0d53239d35f
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 01:44:17 +0000

    Added 4 classes.

---
+++ Lab4/src/mum/cs490/lab4/partB/Mutex.java
@@ -0,0 +1,45 @@
+package mum.cs490.lab4.partB;
+
+public class Mutex {
+    private boolean locked = false;
+
+    public Mutex() {
+    }
+    public Mutex(boolean locked) {
+	this.locked = locked;
+    }
+
+    /** if locked
+     *		then wait 
+     *  	else lock the mutex and continue
+     */
+    public void acquire() {
+	synchronized (this) {
+	    while (locked) {
+		try {
+		    this.wait();
+		} catch (Exception e) {
+		}
+	    }
+	    locked = true;
+	}
+    }
+
+    /** wake up waiting threads and unlock the mutex
+     */
+    public void release() {
+	synchronized (this) {
+	    locked = false;	// set to unlocked
+	    this.notify();	// wake up a waiting thread
+	}
+    }
+
+    /** is the mutex locked
+     */
+    public boolean isLocked() {
+	synchronized (this) {
+	    return locked;
+	}
+    }
+
+} // end of class Mutex
---
+++ Lab4/src/mum/cs490/lab4/partB/Producer.java
@@ -0,0 +1,38 @@
+package mum.cs490.lab4.partB;
+
+public class Producer implements Runnable  {
+
+    // NOT shared by other producers (non-static fields and methods)
+    private int pid;
+    
+    
+
+    public Producer(int pid) {
+	this.pid = pid;
+    }
+    public void run()  {
+	while (true) {
+	    int item = produce_item();
+	    Main.emptySemaphore.down();
+	    Main.mutex.acquire();
+	    insert_item(item);
+	    Main.mutex.release();
+	    Main.fullSemaphore.up();
+	}
+
+    }
+
+    int produce_item() {
+	Main.pause(1000);
+	System.out.println("produce=P"+pid); 
+	return pid;
+    }
+
+    void insert_item(int item) {
+	System.out.println("P" + pid + " inserting '" + item 
+			   + "' into buffer[" + Main.count + "]"); 
+	Main.buffer[Main.count] = item;
+	Main.count++;
+    }
+
+}

ba6cd7d48e48e4dff054d787477ff79b4000b2ef
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 03:16:00 +0000

    Part D initialization.

---
+++ trunk/Lab4/src/mum/cs490/lab4/partD/ConditionQ.java
@@ -0,0 +1,24 @@
+package mum.cs490.lab4.partD;
+
+public interface ConditionQ {
+
+    /** This method causes the current thread to block 
+	until it is awakened by the signal method.
+     */
+    public void await();
+
+    /** If there are threads waiting on 
+	then one of the waiters is signaled (awakened)
+     */
+    public void signal();
+
+    /** If there are threads waiting on this ConditionQ,
+	then all of the waiters are signaled
+     */
+    public void signalAll();
+
+    /** If there are waiters, 
+	this method returns true, otherwise it returns false */
+    public boolean hasWaiters();
+
+}

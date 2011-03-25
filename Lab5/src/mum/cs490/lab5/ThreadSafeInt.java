58a37431daa0df89a5da25c256aabda7f202c28e
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 18:46:27 +0000

    Made the class thread-safe.

--- Lab5/src/mum/cs490/lab5/ThreadSafeInt.java
+++ Lab5/src/mum/cs490/lab5/ThreadSafeInt.java
@@ -1,33 +1,49 @@
 package mum.cs490.lab5;
 
 public class ThreadSafeInt {
-    private int value = 0;
+	private int value = 0;
+	private Lock lock = new Lock();
 
-    public ThreadSafeInt(int initVal) {
-	value = initVal;
-    }
+	public ThreadSafeInt(int initVal) {
+		lock.lock();
+		value = initVal;
+		lock.unlock();
+	}
 
-    /** decrement value
-     */
-    public void dec() {
-	    value--;
-    }
+	/**
+	 * decrement value
+	 */
+	public void dec() {
+		lock.lock();
+		value--;
+		lock.unlock();
+	}
 
-    /** increment value
-     */
-    public void inc() {
-	    value++;		// increment value
-    }
+	/**
+	 * increment value
+	 */
+	public void inc() {
+		lock.lock();
+		value++; // increment value
+		lock.unlock();
+	}
 
-    /** get the value of ThreadSafeInt
-     */
-    public int getValue() {
-	    return value;
-    }
+	/**
+	 * get the value of ThreadSafeInt
+	 */
+	public int getValue() {
+		lock.lock();
+		int returnValue = value;
+		lock.unlock();
+		return returnValue;
+	}
 
-    /** set the value of ThreadSafeInt
-     */
-    public void setValue(int newVal) {
-	value = newVal;
-    }
+	/**
+	 * set the value of ThreadSafeInt
+	 */
+	public void setValue(int newVal) {
+		lock.lock();
+		value = newVal;
+		lock.unlock();
+	}
 }

68d3bc6062d8e33475716e1b3d76c0d7c116bb71
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 19:49:42 +0000

    Re-initialized part D.

---
+++ Lab5/src/mum/cs490/lab5/partD/Consumer.java
@@ -0,0 +1,33 @@
+package mum.cs490.lab5.partD;
+public class Consumer implements Runnable  {
+
+    // NOT shared by other consumers (non-static fields and methods)
+    private int pid;
+
+    public Consumer(int pid) {
+	this.pid = pid;
+    }
+
+    public void run()  {
+	while (true) {
+	    int item = remove_item();
+	    consume_item(item);
+	}
+
+    }
+
+    int remove_item() {
+	Main.count--;
+	int item = Main.buffer[Main.count];
+	System.out.println("C" + pid + " removing '" + item 
+			   + "' from buffer[" + Main.count + "]");
+	Main.buffer[Main.count] = -1;
+	return item;
+    }
+
+    void consume_item(int item) {
+	System.out.println("C" + pid + " consuming " + item); 
+	Main.pause(1000);
+    }
+
+}
---
+++ Lab5/src/mum/cs490/lab5/partD/Main.java
@@ -0,0 +1,34 @@
+package mum.cs490.lab5.partD;
+import java.util.Random;
+
+public class Main {
+
+    static Random random = new Random();
+    static final int producers = 5;
+    static final int consumers = 5;
+
+    public static final int max = 5;
+    public static int count = 0;
+    public static int buffer[] = new int[max];
+
+    static public void main(String[] arg)  {
+
+	for (int i=0; i<producers; i++)  {
+	    Producer c = new Producer(i);
+	    Thread t = new Thread(c);  // create thread for each producer
+	    t.start();
+	}
+
+	for (int i=0; i<consumers; i++)  {
+	    Consumer c = new Consumer(i);
+	    Thread t = new Thread(c);  // create thread for each consumer
+	    t.start();
+	}
+
+    }
+
+    static public void pause(int milliseconds)  {
+	try{Thread.sleep(random.nextInt()%milliseconds);}catch(Exception e){}
+    }
+
+}

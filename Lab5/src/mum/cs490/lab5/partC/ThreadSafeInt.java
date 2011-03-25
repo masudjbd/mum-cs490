063f00752400a20f8beecacad1e6ec78e1ba731e
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 19:16:30 +0000

    Initialization of part C.

---
+++ Lab5/src/mum/cs490/lab5/partC/ThreadSafeInt.java
@@ -0,0 +1,32 @@
+package mum.cs490.lab5.partC;
+public class ThreadSafeInt {
+    private int value = 0;
+
+    public ThreadSafeInt(int initVal) {
+	value = initVal;
+    }
+
+    /** decrement value
+     */
+    public void dec() {
+	    value--;
+    }
+
+    /** increment value
+     */
+    public void inc() {
+	    value++;		// increment value
+    }
+
+    /** get the value of ThreadSafeInt
+     */
+    public int getValue() {
+	    return value;
+    }
+
+    /** set the value of ThreadSafeInt
+     */
+    public void setValue(int newVal) {
+	value = newVal;
+    }
+}

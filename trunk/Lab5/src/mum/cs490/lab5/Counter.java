1db876af97e6d2f4fdab13946fbafce8a86523ea
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 18:58:09 +0000

    Protected critical sections in Counter with Lock.

--- trunk/Lab5/src/mum/cs490/lab5/Counter.java
+++ trunk/Lab5/src/mum/cs490/lab5/Counter.java
@@ -1,43 +1,56 @@
 package mum.cs490.lab5;
 
-public class Counter implements Runnable  {
-    static int count = 0;
-    static final int threadCnt = 10;
-    static Thread threads[] = new Thread[threadCnt];
+public class Counter implements Runnable {
+	// static int count = 0;
+	static ThreadSafeInt newCount = new ThreadSafeInt(0);
+	static final int threadCnt = 100;
+	static Thread threads[] = new Thread[threadCnt];
 
-    static public void main(String[] arg)  {
+	static Lock lock = new Lock();
 
-	System.out.println(); 
-	for (int i=0; i<threadCnt; i++)  {
-	    Counter c = new Counter (i);
-	    threads[i] = new Thread(c);  // create thread for each counter
-	    threads[i].start();
+	static public void main(String[] arg) {
+
+		System.out.println();
+		for (int i = 0; i < threadCnt; i++) {
+			Counter c = new Counter(i);
+			threads[i] = new Thread(c); // create thread for each counter
+			threads[i].start();
+		}
+		for (int i = 0; i < threadCnt; i++) {
+			try {
+				threads[i].join();
+			} catch (Exception e) {
+			}
+		}
+
+		// System.out.println("Count="+count);
+		System.out.println("Count=" + newCount.getValue());
 	}
-	for (int i=0; i<threadCnt; i++)  {
-	    try {
-		threads[i].join();
-	    } catch(Exception e) { }
+
+	private int tid;
+
+	public Counter(int tid) {
+		this.tid = tid;
 	}
 
-	System.out.println("Count="+count);
-    }
+	public void run() {
+		for (int i = 0; i < 100; i++) {
 
-    private int tid;
+			lock.lock();
+			System.out.printf("%4d", tid); // print pid followed by a space
+			// count = count + 1;
 
-    public Counter(int tid) {
-	this.tid = tid;
-    }
-    public void run()  {
-	for (int i=0; i<100; i++)  {
+			newCount.inc();
+			// if (count % 25 == 0) { // print 50 characters per line
+			// System.out.println();
+			// }
+			if (newCount.getValue() % 25 == 0) { // print 50 characters per line
+				System.out.println();
+			}
+			lock.unlock();
 
-		System.out.print(tid + " "); // print pid followed by a space
-		count = count + 1;
-		if (count % 25 == 0) {    // print 50 characters per line
-		    System.out.println(); 
 		}
 
 	}
-
-    }
 
 }

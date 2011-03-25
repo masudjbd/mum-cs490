208ed7627c3cc6958cf295250bc389bc7a602818
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 01:44:17 +0000

    Added 4 classes.

---
+++ trunk/Lab4/src/mum/cs490/lab4/partB/Consumer.java
@@ -0,0 +1,38 @@
+package mum.cs490.lab4.partB;
+
+public class Consumer implements Runnable  {
+
+    // NOT shared by other consumers (non-static fields and methods)
+    private int pid;
+
+    public Consumer(int pid) {
+	this.pid = pid;
+    }
+
+    public void run()  {
+	while (true) {
+	    Main.fullSemaphore.down();
+	    Main.mutex.acquire();
+		int item = remove_item();
+		Main.mutex.release();
+	    Main.emptySemaphore.up();
+		consume_item(item);
+	}
+
+    }
+
+    int remove_item() {
+	Main.count--;
+	int item = Main.buffer[Main.count];
+	System.out.println("C" + pid + " removing '" + item 
+			   + "' from buffer[" + Main.count + "]");
+	Main.buffer[Main.count] = -1;
+	return item;
+    }
+
+    void consume_item(int item) {
+	System.out.println("C" + pid + " consuming " + item); 
+	Main.pause(1000);
+    }
+
+}

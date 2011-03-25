a64ff746edc82d1807e5e53046103b03d4af0e09
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 18:40:34 +0000

    Initial import.

---
+++ trunk/Lab5/src/mum/cs490/lab5/ThreadSafeIntLock.java
@@ -0,0 +1,33 @@
+package mum.cs490.lab5;
+
+public class ThreadSafeIntLock {
+    private int value = 0;
+
+    public ThreadSafeIntLock(int initVal) {
+	value = initVal;
+    }
+
+    /** decrement value
+     */
+    public void dec() {
+	    value--;
+    }
+
+    /** increment value
+     */
+    public void inc() {
+	    value++;		// increment value
+    }
+
+    /** get the value of ThreadSafeInt
+     */
+    public int getValue() {
+	    return value;
+    }
+
+    /** the value of ThreadSafeInt
+     */
+    public void setValue(int newVal) {
+	value = newVal;
+    }
+}

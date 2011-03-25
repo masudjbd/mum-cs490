10c99ed7c20930870402aa3720023c8c044d0466
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 19:07:00 +0000

    Made the class thread-safe with synchronized blocks.

--- trunk/Lab5/src/mum/cs490/lab5/partB/ThreadSafeInt.java
+++ trunk/Lab5/src/mum/cs490/lab5/partB/ThreadSafeInt.java
@@ -1,32 +1,47 @@
 package mum.cs490.lab5.partB;
+
 public class ThreadSafeInt {
-    private int value = 0;
+	private int value = 0;
 
-    public ThreadSafeInt(int initVal) {
-	value = initVal;
-    }
+	public ThreadSafeInt(int initVal) {
+		synchronized (this) {
+			value = initVal;
+		}
+	}
 
-    /** decrement value
-     */
-    public void dec() {
-	    value--;
-    }
+	/**
+	 * decrement value
+	 */
+	public void dec() {
+		synchronized (this) {
+			value--;
+		}
+	}
 
-    /** increment value
-     */
-    public void inc() {
-	    value++;		// increment value
-    }
+	/**
+	 * increment value
+	 */
+	public void inc() {
+		synchronized (this) {
+			value++; // increment value
+		}
+	}
 
-    /** get the value of ThreadSafeInt
-     */
-    public int getValue() {
-	    return value;
-    }
+	/**
+	 * get the value of ThreadSafeInt
+	 */
+	public int getValue() {
+		synchronized (this) {
+			return value;
+		}
+	}
 
-    /** set the value of ThreadSafeInt
-     */
-    public void setValue(int newVal) {
-	value = newVal;
-    }
+	/**
+	 * set the value of ThreadSafeInt
+	 */
+	public void setValue(int newVal) {
+		synchronized (this) {
+			value = newVal;
+		}
+	}
 }

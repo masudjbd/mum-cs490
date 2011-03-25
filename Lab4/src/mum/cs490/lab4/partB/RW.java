f1e48f5ae3a5373846d1461fbb473bbcf18ce5d1
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-25 01:36:52 +0000

    Initial import.

---
+++ Lab4/src/mum/cs490/lab4/partB/RW.java
@@ -0,0 +1,30 @@
+package mum.cs490.lab4.partB;
+import java.util.Random;
+
+public class RW {
+    // mutex is for critical regions
+    public static final Semaphore mutex = new Semaphore(1);
+
+    // controls write access to the DB
+    public static final Semaphore db = new Semaphore(1);
+
+    public static int rc = 0;		// number of threads reading the DB
+
+    static Random random = new Random();
+
+    public static void main(String[] arg)  {
+	for (int i=0; i<5; i++)  { 
+	    Reader reader = new Reader(i);
+	    new Thread(reader).start();  // create thread for each reader 
+	}
+	for (int i=0; i<3; i++)  {
+	    Writer writer = new Writer(i);
+	    new Thread(writer).start();  // create thread for each writer
+	}
+    }
+
+    static public void pause(int milliseconds)  {
+	try{Thread.sleep(random.nextInt()%milliseconds);}catch(Exception e){}
+    }
+
+}

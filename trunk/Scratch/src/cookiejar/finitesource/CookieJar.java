03f16e61a1f334959ec8766583ee47d9f1c074d9
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-31 01:44:03 +0000

    Cookie Jar problems solved - both unbounded and bounded cookie jars.

--- trunk/Scratch/src/cookiejar/finitesource/CookieJar.java
+++ trunk/Scratch/src/cookiejar/finitesource/CookieJar.java
@@ -13,12 +13,32 @@ public class CookieJar {
 	static Random random = new Random();
 
 	public static void main(String... args) {
+		new Thread(new Mother()).start();
 		new Thread(new Tina()).start();
 		new Thread(new Judy()).start();
 	}
 
+	private static void checkCookieCount() {
+		if (cookieCount == 0) {
+			synchronized (mother) {
+				System.out.println("Oops! no cookie mother!!! Please help.");
+				mother.notifyAll();
+			}
+			try {
+				System.out
+						.println("We are hungry mother. Where are other cookies? We are waiting.");
+				synchronized (children) {
+					children.wait();
+				}
+			} catch (InterruptedException e) {
+			}
+
+		}
+
+	}
+
 	private static void randomWait() {
-		int millisecs = random.nextInt(2);
+		int millisecs = random.nextInt(100);
 		try {
 			Thread.sleep(millisecs);
 		} catch (InterruptedException e) {
@@ -31,19 +51,33 @@ public class CookieJar {
 		public void run() {
 			while (true) {
 				randomWait();
+				boolean empty = false;
 				synchronized (o) {
 					if (cookieCount == 0) {
+						empty = true;
 						synchronized (mother) {
+							System.out
+									.println("Oops! no cookie mother!!! Please help.");
 							mother.notifyAll();
 						}
-						try {
-							children.wait();
-						} catch (InterruptedException e) {
-						}
 					}
+				}
+				if (empty) {
+					try {
+						System.out
+								.println("We are hungry mother. Where are other cookies? We are waiting.");
+						synchronized (children) {
+							children.wait();
+						}
+					} catch (InterruptedException e) {
+					}
+				}
+				synchronized (o) {
+
+					cookieCount--;
 					tinaCount++;
 					System.out.println("TinaCount = " + tinaCount);
-					o.notifyAll();
+					o.notify();
 				}
 				System.out.println("Tina is eating cookie.");
 			}
@@ -58,8 +92,10 @@ public class CookieJar {
 		public void run() {
 			while (true) {
 				randomWait();
+				boolean empty = false;
 				synchronized (o) {
 					System.out.println("Judy tries to get a cookie.");
+
 					while (tinaCount < 2) {
 						System.out
 								.println("Judy is waiting since Tina has not eaten 2 times.");
@@ -70,9 +106,35 @@ public class CookieJar {
 						} catch (InterruptedException e) {
 						}
 					}
+					// start of method
+
+					if (cookieCount == 0) {
+						empty = true;
+						synchronized (mother) {
+							System.out
+									.println("Oops! no cookie mother!!! Please help.");
+							mother.notify();
+						}
+					}
+				}
+
+				if (empty) {
+					try {
+						System.out
+								.println("We are hungry mother. Where are other cookies? We are waiting.");
+						synchronized (children) {
+							children.wait();
+						}
+					} catch (InterruptedException e) {
+					}
+				}
+				// end of method
+				synchronized (o) {
+
 				}
 				System.out.println("Judy is eating cookie.");
 				synchronized (o) {
+					cookieCount--;
 					tinaCount = 0;
 					System.out.println("Judy has now reset the tinaCount.");
 				}
@@ -83,17 +145,29 @@ public class CookieJar {
 	}
 
 	static class Mother implements Runnable {
-		private static void mother() {
-			synchronized (o) {
-				cookieCount = 25;
-				System.out
-						.println("Mother has filled the cookie jar with good cookies. Hurray!!!");
-			}
-		}
 
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
+			while (true) {
+				synchronized (mother) {
+					try {
+						System.out
+								.println("Mother is sleeping children. Wake me up when you finish your cookies.");
+						mother.wait();
+					} catch (InterruptedException e) {
+					}
+				}
+				synchronized (o) {
+					cookieCount = 25;
+					System.out
+							.println("Mother has filled the cookie jar with good cookies.");
+				}
+				synchronized (children) {
+					System.out
+							.println("Get the cookies dear! I've filled up the jar.");
+					children.notifyAll();
+				}
+			}
 
 		}
 

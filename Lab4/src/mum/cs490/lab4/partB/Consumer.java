c467792f820e711143c815245c94843dd6b2ec92
ashishadhikari@gmail.com <ashishadhikari@gmail.com@a5e79142-11e1-0cd7-a7a7-42aa866e8b12>
2011-03-31 02:11:52 +0000

    Made compatible with Professor's new Lock file.

--- Lab4/src/mum/cs490/lab4/partB/Consumer.java
+++ Lab4/src/mum/cs490/lab4/partB/Consumer.java
@@ -1,42 +1,43 @@
 package mum.cs490.lab4.partB;
 
-public class Consumer implements Runnable  {
+public class Consumer implements Runnable {
 
-    // NOT shared by other consumers (non-static fields and methods)
-    private int pid;
+	// NOT shared by other consumers (non-static fields and methods)
+	private int pid;
 
-    public Consumer(int pid) {
-	this.pid = pid;
-    }
-
-    public void run()  {
-	while (true) {
-//	    Main.fullSemaphore.down();
-//	    Main.mutex.acquire();
-		Main.lock.lock();
-		Main.fullQ.await();
-		int item = remove_item();
-//		Main.mutex.release();
-//	    Main.emptySemaphore.up();
-		Main.emptyQ.signal();
-		Main.lock.unlock();
-		consume_item(item);
+	public Consumer(int pid) {
+		this.pid = pid;
 	}
 
-    }
+	public void run() {
+		while (true) {
+			// Main.fullSemaphore.down();
+			// Main.mutex.acquire();
+			Main.lock.lock();
+			while (Main.count == 0)
+				Main.fullQ.await();
+			int item = remove_item();
+			// Main.mutex.release();
+			// Main.emptySemaphore.up();
+			Main.emptyQ.signal();
+			Main.lock.unlock();
+			consume_item(item);
+		}
 
-    int remove_item() {
-	Main.count--;
-	int item = Main.buffer[Main.count];
-	System.out.println("C" + pid + " removing '" + item 
-			   + "' from buffer[" + Main.count + "]");
-	Main.buffer[Main.count] = -1;
-	return item;
-    }
+	}
 
-    void consume_item(int item) {
-	System.out.println("C" + pid + " consuming " + item); 
-	Main.pause(1000);
-    }
+	int remove_item() {
+		Main.count--;
+		int item = Main.buffer[Main.count];
+		System.out.println("C" + pid + " removing '" + item + "' from buffer["
+				+ Main.count + "]");
+		Main.buffer[Main.count] = -1;
+		return item;
+	}
+
+	void consume_item(int item) {
+		System.out.println("C" + pid + " consuming " + item);
+		Main.pause(1000);
+	}
 
 }
--- Lab4/src/mum/cs490/lab4/partB/Producer.java
+++ Lab4/src/mum/cs490/lab4/partB/Producer.java
@@ -1,42 +1,42 @@
 package mum.cs490.lab4.partB;
 
-public class Producer implements Runnable  {
+public class Producer implements Runnable {
 
-    // NOT shared by other producers (non-static fields and methods)
-    private int pid;
-    
-    
+	// NOT shared by other producers (non-static fields and methods)
+	private int pid;
 
-    public Producer(int pid) {
-	this.pid = pid;
-    }
-    public void run()  {
-	while (true) {
-	    int item = produce_item();
-//	    Main.emptySemaphore.down();
-//	    Main.mutex.acquire();
-	    Main.lock.lock();
-	    Main.emptyQ.await();
-	    insert_item(item);
-//	    Main.mutex.release();
-//	    Main.fullSemaphore.up();
-	    Main.fullQ.signal();
-	    Main.lock.unlock();
+	public Producer(int pid) {
+		this.pid = pid;
 	}
 
-    }
+	public void run() {
+		while (true) {
+			int item = produce_item();
+			// Main.emptySemaphore.down();
+			// Main.mutex.acquire();
+			Main.lock.lock();
+			while (Main.count == Main.max)
+				Main.emptyQ.await();
+			insert_item(item);
+			// Main.mutex.release();
+			// Main.fullSemaphore.up();
+			Main.fullQ.signal();
+			Main.lock.unlock();
+		}
 
-    int produce_item() {
-	Main.pause(1000);
-	System.out.println("produce=P"+pid); 
-	return pid;
-    }
+	}
 
-    void insert_item(int item) {
-	System.out.println("P" + pid + " inserting '" + item 
-			   + "' into buffer[" + Main.count + "]"); 
-	Main.buffer[Main.count] = item;
-	Main.count++;
-    }
+	int produce_item() {
+		Main.pause(1000);
+		System.out.println("produce=P" + pid);
+		return pid;
+	}
+
+	void insert_item(int item) {
+		System.out.println("P" + pid + " inserting '" + item + "' into buffer["
+				+ Main.count + "]");
+		Main.buffer[Main.count] = item;
+		Main.count++;
+	}
 
 }
